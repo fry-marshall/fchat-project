7f5f52048fca55d7a588d7bde780394a44d5d95b
Marshall FRY <marshalfry1998@gmail.com>
2023-12-03 00:19:36 +0100

    add unit tests for message endpoints

--- api-fchat/src/app/controllers/message-controller.ts
+++ api-fchat/src/app/controllers/message-controller.ts
@@ -9,6 +9,12 @@ import Conversation from "../models/conversation";
 import { v4 as uuidv4 } from "uuid";
 import { Op } from "sequelize";
 
+Conversation.belongsTo(User, { foreignKey: 'user_id_1', as: 'User1' })
+Conversation.belongsTo(User, { foreignKey: 'user_id_2', as: 'User2' })
+Message.belongsTo(User, { foreignKey: 'sender_id', as: 'Sender' })
+Message.belongsTo(User, { foreignKey: 'receiver_id', as: 'Receiver' })
+Message.belongsTo(Conversation, { foreignKey: 'conversation_id', as: 'Conversation' })
+
 
 class MessageController extends Controller {
 
@@ -57,7 +63,7 @@ class MessageController extends Controller {
                 )
             }
 
-            return res.status(201).json(Helpers.queryResponse(conversations))
+            return res.status(200).json(Helpers.queryResponse(response))
 
         } catch (e) {
             return res.status(500).json(Helpers.serverError)
@@ -70,6 +76,8 @@ class MessageController extends Controller {
 
             const user = res.locals.user;
             const receiverUserId = req.body?.receiver_id;
+
+            console.log("yoooooooooooooo")
 
             if (receiverUserId === user.id) {
                 return res.status(400).json(Helpers.queryResponse({ msg: 'Receiver user cannot be the sender user' }))
@@ -99,34 +107,31 @@ class MessageController extends Controller {
             if(!conversation){
                 const body = {
                     id: uuidv4(),
-                    user_id_1: receiverUserId,
-                    user_id_2: user.id
                 }
                 conversation = await Conversation.create(body)
-
+                await conversation.setUser1(user)
+                await conversation.setUser2(receiverUser)
             }
 
-            const response = await this.service.insert({
+            const message = await Message.create({
                 id: uuidv4(),
                 content: req.body?.content,
-                sender_id: user.id,
-                receiver_id: receiverUserId,
-                conversation_id: conversation.id
             })
 
-            if (response.is_error){
-                return res.status(400).send(response);
-            }
+            await message.setSender(user)
+            await message.setReceiver(receiverUser)
+            await message.setConversation(conversation)
 
             res.locals.io.emit(receiverUserId, {
                 conversation_id: conversation.id,
-                content: (response as any).data.content,
-                date: (response as any).data.date
+                content: message.content,
+                date: message.date
             })
 
-            return res.status(201).json(Helpers.queryResponse({conversation_id: conversation.id, message_id: (response as any).data.id, msg: 'msg sent successfully' }))
+            return res.status(201).json(Helpers.queryResponse({conversation_id: conversation.id, message_id: message.id, msg: 'msg sent successfully' }))
 
         } catch (e) {
+            console.log(e)
             return res.status(500).json(Helpers.serverError)
         }
     }
--- api-fchat/src/app/routes/router.ts
+++ api-fchat/src/app/routes/router.ts
@@ -4,7 +4,14 @@ import { Express} from "express"
 import { Server } from "socket.io";
 
 const router = (app: Express, io: Server) => {
-	app.use("/message", messageRouter)
+	app.use(
+		"/message", 
+		(req, res, next)=>{
+			res.locals.io = io
+			next()
+		},
+		messageRouter
+	)
 	app.use(
 		"/user", 
 		(req, res, next)=>{
