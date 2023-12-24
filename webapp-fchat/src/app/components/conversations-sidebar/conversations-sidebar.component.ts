791edba03362b4838a4823a20aa1b9e8d5af7913
Marshall FRY <marshalfry1998@gmail.com>
2023-12-24 17:37:43 +0100

    Add options & redirections

--- webapp-fchat/src/app/components/conversations-sidebar/conversations-sidebar.component.ts
+++ webapp-fchat/src/app/components/conversations-sidebar/conversations-sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from '@library_v2/interfaces/user';
+import { BehaviorSubject } from 'rxjs';
 import { MessageFacade } from 'src/app/stores/message/message.facade';
 import { Conversation } from 'src/app/stores/message/message.interface';
 import { ViewsService } from 'src/app/views/views.service';
@@ -14,15 +16,33 @@ export class ConversationsSidebarComponent{
   @Input() conversations: Conversation[] = [];
   @Input() currentUser: User;
   @Input() allUsers: User[];
-
+  
+  isLoading: BehaviorSubject<boolean> = new BehaviorSubject(false)
   conversationFiltered: Conversation[] = this.conversations;
-
   filterUserName: string
+  options = [
+    "Changer le mot de passe",
+    "Se déconnecter",
+    "Supprimer mon compte"
+  ]
+  displayChangePasswordModal = false;
+  displayLogOutModal = false;
+  displayDeleteAccountModal = false;
+
+  formChangePassword: FormGroup = new FormGroup({
+    old_password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    new_password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirm_new_password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  })
 
   constructor(
     private viewsService: ViewsService,
     private messageFacade: MessageFacade
   ){}
+
+  get oldPassword() { return this.formChangePassword.get('old_password'); }
+  get newPassword() { return this.formChangePassword.get('new_password'); }
+  get confirmNewPassword() { return this.formChangePassword.get('confirm_new_password'); }
 
   showNewMessage(){
     this.viewsService.updateShowConvList(false)
@@ -38,5 +58,39 @@ export class ConversationsSidebarComponent{
 
   filterConversation(){
     this.conversationFiltered = this.conversations.filter(conv => this.getUserInfos(conv)?.fullname.includes(this.filterUserName) || this.getUserInfos(conv)?.fullname === null)
+  }
+
+  optionSelected(option: string){
+    switch(option){
+      case this.options[0]:
+        this.displayChangePasswordModal = true;
+        this.displayDeleteAccountModal = false;
+        this.displayLogOutModal = false;
+        break;
+
+      case this.options[1]:
+        this.displayChangePasswordModal = false;
+        this.displayDeleteAccountModal = false;
+        this.displayLogOutModal = true;
+        break;
+
+      case this.options[2]:
+        this.displayChangePasswordModal = false;
+        this.displayDeleteAccountModal = true;
+        this.displayLogOutModal = false;
+        break;
+    }
+  }
+
+  changePassword(){
+
+  }
+
+  deleteAccount(){
+
+  }
+
+  logOutUser(){
+
   }
 }
