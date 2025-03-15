c1a57e54f267372c93f5fb46b6fdf892d140f241
Marshall FRY <marshalfry1998@gmail.com>
2025-03-15 16:20:34 +0100

    add database config

--- api-fchat-new/src/app.module.ts
+++ api-fchat-new/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const NODE_ENV = process.env.NODE_ENV || 'dev';
 
@@ -11,15 +12,27 @@ const NODE_ENV = process.env.NODE_ENV || 'dev';
     ConfigModule.forRoot({
       envFilePath: `.env${NODE_ENV}`,
       validationSchema: Joi.object({
-        PORT: Joi.number().default(3000),
+        LOCAL_PORT: Joi.number().default(3000),
         DOCKER_PORT: Joi.number().default(3000),
         ACCESS_TOKEN_SECRET: Joi.string().required(),
         REFRESH_TOKEN_SECRET: Joi.string().required(),
-        DATABASE_HOST: Joi.string().required(),
-        DATABASE_NAME: Joi.string().required(),
-        DATABASE_USERNAME: Joi.string().required(),
-        DATABASE_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_LOCAL_PORT: Joi.number().default(5432),
+        DB_DOCKER_PORT: Joi.number().default(5432),
       }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'db',
+      port: parseInt(process.env.DB_DOCKER_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: NODE_ENV === 'dev',
     }),
   ],
   controllers: [AppController],
