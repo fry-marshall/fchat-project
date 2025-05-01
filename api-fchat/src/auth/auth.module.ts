bfc1ea86dfe8eb55dea35a8a72764da607359610
Marshall FRY <marshalfry1998@gmail.com>
2025-05-02 00:15:00 +0200

    fix env uploading

--- api-fchat/src/auth/auth.module.ts
+++ api-fchat/src/auth/auth.module.ts
@@ -10,29 +10,34 @@ import { JwtStrategy } from './strategies/jwt-strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/users/users.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.MAIL_HOST,
-        port: parseInt(process.env.MAIL_PORT!),
-        secure: false,
-        auth: {
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get<string>('MAIL_HOST'),
+          port: config.get<number>('MAIL_PORT'),
+          secure: false,
+          auth: {
+            user: config.get<string>('MAIL_USERNAME'),
+            pass: config.get<string>('MAIL_PASSWORD'),
+          },
         },
-      },
-      defaults: {
-        from: '"FChat" <marshalfry1998@gmail.com>',
-      },
-      template: {
-        dir: __dirname + '../../../templates',
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
+        defaults: {
+          from: '"FChat" <marshalfry1998@gmail.com>',
         },
-      },
+        template: {
+          dir: __dirname + '../../../templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
     }),
     CommonModule,
     PassportModule,
