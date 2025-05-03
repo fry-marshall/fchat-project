1b757cabd7cff50600de818acbe41a0fbf59302e
Marshall FRY <marshalfry1998@gmail.com>
2025-05-03 15:31:03 +0200

    update api url

--- webapp-fchat/src/environments/environment.prod.ts
+++ webapp-fchat/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api-fchat.mfry.io/',
-    authUrl: 'https://auth-fchat.mfry.io',
+    apiUrl: 'http://api-fchat.mfry.io/',
+    authUrl: 'http://auth-fchat.mfry.io',
     assetsUrl: 'https://tassa.nyc3.cdn.digitaloceanspaces.com/fchat/',
 };
