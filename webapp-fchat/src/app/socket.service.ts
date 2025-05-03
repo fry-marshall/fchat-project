bd562d20587e04630109d587275d24b6f4daf6bd
Marshall FRY <marshalfry1998@gmail.com>
2025-05-03 18:32:47 +0200

    add websocket service

---
+++ webapp-fchat/src/app/socket.service.ts
@@ -0,0 +1,31 @@
+// socket.service.ts
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class SocketService {
+  private socket: Socket;
+
+  constructor(private cookieService: CookieService) {
+    const token = this.cookieService.get('access_token');
+    this.socket = io(environment.apiUrl, {
+      transports: ['websocket'],
+      auth: {
+        token,
+      },
+    });
+  }
+
+  listen<T>(event: string) {
+    return new Observable<T>((subscriber) => {
+      this.socket.on(event, (data: T) => subscriber.next(data));
+    });
+  }
+
+  emit(event: string, data: any) {
+    this.socket.emit(event, data);
+  }
+}
