ec6af9265a3de3e374d3dee0b140bade184b1ab5
Marshall FRY <marshalfry1998@gmail.com>
2025-05-06 03:22:56 +0200

    update api to https

--- webapp-fchat/src/environments/environment.dev.ts
+++ webapp-fchat/src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  authUrl: 'http://toto.com',
-  apiUrl: 'http://api-fchat.mfry.io/',
-  appUrl: 'http://webapp-fchat.mfry.io/',
+  authUrl: 'https://auth-fchat.mfry.io/',
+  apiUrl: 'https://api-fchat.mfry.io/',
+  appUrl: 'https://webapp-fchat.mfry.io/',
   assetsUrl: 'https://tassa.nyc3.cdn.digitaloceanspaces.com/fchat/',
 };
--- webapp-fchat/src/environments/environment.prod.ts
+++ webapp-fchat/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://api-fchat.mfry.io/',
-    authUrl: 'http://auth-fchat.mfry.io',
+    apiUrl: 'https://api-fchat.mfry.io/',
+    authUrl: 'https://auth-fchat.mfry.io',
+    appUrl: 'https://webapp-fchat.mfry.io/',
     assetsUrl: 'https://tassa.nyc3.cdn.digitaloceanspaces.com/fchat/',
 };
